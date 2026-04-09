325ba8e0907ad7131f8584eef3b48cb1619bed40
tejasveersmatharu-debug <tejasveersmatharu@gmail.com>
2026-04-09 18:08:42 +0530

    changing the caps

--- 3-myntra-react-clone/src/routes/App.jsx
+++ 3-myntra-react-clone/src/routes/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../components/footer";
+import Header from "../components/header";
 import FetchItems from "../components/fetchItems";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";

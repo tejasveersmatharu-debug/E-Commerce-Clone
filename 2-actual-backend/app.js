d80f05aa0b6a4775122c0e85a2330b46a5bbff55
tejasveersmatharu-debug <tejasveersmatharu@gmail.com>
2026-04-09 17:26:59 +0530

    fix: render deployment

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -1,33 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const { getStoredItems, storeItems } = require('./data/items');
-
+const { getStoredItems, storeItems } = require("./data/items");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.get('/items', async (req, res) => {
+app.get("/items", async (req, res) => {
   const storedItems = await getStoredItems();
   // await new Promise((resolve, reject) => setTimeout(() => resolve(), 4000));
   res.json({ items: storedItems });
 });
 
-app.get('/items/:id', async (req, res) => {
+app.get("/items/:id", async (req, res) => {
   const storedItems = await getStoredItems();
   const item = storedItems.find((item) => item.id === req.params.id);
   res.json({ item });
 });
 
-app.post('/items', async (req, res) => {
+app.post("/items", async (req, res) => {
   const existingItems = await getStoredItems();
   const itemData = req.body;
   const newItem = {
@@ -36,7 +35,10 @@ app.post('/items', async (req, res) => {
   };
   const updatedItems = [newItem, ...existingItems];
   await storeItems(updatedItems);
-  res.status(201).json({ message: 'Stored new item.', item: newItem });
+  res.status(201).json({ message: "Stored new item.", item: newItem });
 });
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
